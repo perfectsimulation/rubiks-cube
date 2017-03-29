a88c2f3f5eb5f3844bae352a787069470672bfaf
Sally Blair <sallyblair88@gmail.com>
2017-03-29 00:33:48 -0400

    Update Save.cs

--- UI/Save.cs
+++ UI/Save.cs
@@ -1,14 +1,24 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
+/// <summary>
+/// This class contains methods to save a configuration of the Rubik's Cube.
+/// </summary>
 public class Save : MonoBehaviour {
 
-	#region globalVariables
+	#region Properties
 
+	// The GameObject variable |solver| contains the script to solve the Rubik's Cube. It is used in
+	// this script to check if an animated solution is active. The solution must be paused for successful
+	// loading.
 	public GameObject solver;
+
+	// The GameObject variable |hint| contains the script to display an animated hint of the rotation
+	// the user should perform. The hint will be deactivated prior to loading.
 	public GameObject hint;
 
+	// The following GameObject variables beginning with "cube" are the pieces of the Rubik's Cube.
 	public GameObject cube00;
 	public GameObject cube01;
 	public GameObject cube02;
@@ -37,11 +47,19 @@ public class Save : MonoBehaviour {
 	public GameObject cube25;
 	public GameObject cube26;
 
+	// The string variable |fileNumber| is a reference to the load file.
 	private string fileNumber;
+
+	// The List<GameObject> variable |allCubes| contains all the pieces of the Rubik's CUbe.
 	private List<GameObject> allCubes  = new List<GameObject> ();
+
+	// The List<Quaternion> variable |allCubesRotations| contains the rotations of each piece.
 	private List<Quaternion> allCubesRotations = new List<Quaternion> ();
 	#endregion
 
+	/// <summary>
+	/// Start this instance by populating |allCubes| and setting the name of the save file.
+	/// </summary>
 	void Start () {
 		fileNumber = this.name [4].ToString ();
 		allCubes.Add (cube00);
@@ -73,26 +91,42 @@ public class Save : MonoBehaviour {
 		allCubes.Add (cube26);
 	}
 
+	/// <summary>
+	/// Gets all the pieces of the Rubik's Cube.
+	/// </summary>
+	/// <returns>The cubes.</returns>
 	List<GameObject> GetCubes () {
 		return allCubes;
 	}
 
+	/// <summary>
+	/// Starts the coroutine to save the current configuration of the Rubik's Cube.
+	/// </summary>
 	public void SaveSignal () {
 		StartCoroutine (SaveState ());
 	}
 
+	/// <summary>
+	/// Saves the current configuration of the Rubik's Cube.
+	/// </summary>
 	IEnumerator SaveState () {
 		bool turning = solver.GetComponent<Solver> ().IsTurning ();
 		bool solving = solver.GetComponent<Solver> ().IsSolving ();
+
+		// There can be no active rotation or solution for the configuration to save.
 		while (turning || solving) {
 			yield return null;
 		}
+
+		// If there is an active hint animation, turn it off before loading.
 		bool hinting = hint.GetComponent<Hint> ().IsHinting ();
 		if (hinting) {
 			hint.GetComponent<Hint> ().ShowOrHide ();
 		}
 		allCubesRotations.Clear ();
 		allCubes = GetCubes ();
+
+		// Populates |allCubesRotations|.
 		foreach (GameObject cube in allCubes) {
 			Quaternion rot = cube.transform.rotation;
 			allCubesRotations.Add (rot);
@@ -100,6 +134,10 @@ public class Save : MonoBehaviour {
 		print ("Saved State " + fileNumber);
 	}
 
+	/// <summary>
+	/// Gets the rotations for each piece of the saved configuration.
+	/// </summary>
+	/// <returns>The rotations for each piece of the saved configuration.</returns>
 	public List<Quaternion> GetCubesRotations () {
 		return allCubesRotations;
 	}
