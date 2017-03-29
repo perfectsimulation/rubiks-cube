1499d257cc2865d5518b1053a3565963e80bf38f
Sally Blair <sallyblair88@gmail.com>
2017-03-29 00:27:05 -0400

    Update Load.cs

--- UI/Load.cs
+++ UI/Load.cs
@@ -1,14 +1,27 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
+/// <summary>
+/// This class contains methods to load a saved configuration of the Rubik's Cube.
+/// </summary>
 public class Load : MonoBehaviour {
 
 	#region globalVariables
+
+	// The GameObject variable |save| is the corresponding save file to be loaded.
 	public GameObject save;
+
+	// The GameObject variable |solver| contains the script to solve the Rubik's Cube. It is used in
+	// this script to check if an animated solution is active. The solution must be paused for successful
+	// loading.
 	public GameObject solver;
+
+	// The GameObject variable |hint| contains the script to display an animated hint of the rotation
+	// the user should perform. The hint will be deactivated prior to loading.
 	public GameObject hint;
 
+	// The following GameObject variables beginning with "cube" are the pieces of the Rubik's Cube.
 	public GameObject cube00;
 	public GameObject cube01;
 	public GameObject cube02;
@@ -37,10 +50,16 @@ public class Load : MonoBehaviour {
 	public GameObject cube25;
 	public GameObject cube26;
 
+	// The string variable |fileNumber| is a reference to the load file.
 	private string fileNumber;
+
+	// The List<GameObject> variable |allCubes| contains all the pieces of the Rubik's CUbe.
 	private List<GameObject> allCubes  = new List<GameObject> ();
 	#endregion
 
+	/// <summary>
+	/// Start this instance by populating |allCubes| and setting the name of the load file.
+	/// </summary>
 	void Start () {
 		fileNumber = this.name [4].ToString ();
 		allCubes.Add (cube00);
@@ -72,23 +91,36 @@ public class Load : MonoBehaviour {
 		allCubes.Add (cube26);
 	}
 
+	/// <summary>
+	/// Starts the coroutine to load a configuration of the Rubik's Cube.
+	/// </summary>
 	public void LoadSignal () {
 		StartCoroutine (LoadState ());
 	}
 
+	/// <summary>
+	/// Loads the configuration of the Rubik's Cube.
+	/// </summary>
 	IEnumerator LoadState () {
-
 		bool turning = solver.GetComponent<Solver> ().IsTurning ();
 		bool solving = solver.GetComponent<Solver> ().IsSolving ();
+
+		// There can be no active rotation or solution for the configuration to load.
 		while (turning || solving) {
 			yield return null;
 		}
+
+		// If there is an active hint animation, turn it off before loading.
 		bool hinting = hint.GetComponent<Hint> ().IsHinting ();
 		if (hinting) {
 			hint.GetComponent<Hint> ().ShowOrHide ();
 		}
+
+		// Retrieves the list of Quaternions of the saved configuration.
 		List<Quaternion> load = save.GetComponent<Save> ().GetCubesRotations ();
 		int index = 0;
+
+		// Set the rotation of each cube in |allCubes|.
 		foreach (Quaternion rot in load) {
 			GameObject cubeID = allCubes [index];
 			cubeID.transform.rotation = rot;
