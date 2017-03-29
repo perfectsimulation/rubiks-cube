8cf0e3fe436ec632e278f506608f3f6da4022797
Sally Blair <sallyblair88@gmail.com>
2017-03-29 14:42:23 -0400

    Update Pause.cs

--- UI/Pause.cs
+++ UI/Pause.cs
@@ -1,10 +1,17 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// This script stops the the animated solution.
+/// </summary>
 public class Pause : MonoBehaviour {
 
+	// The GameObject variable |solver| contains the script to solve the Rubik's Cube.
 	public GameObject solver;
 
+	/// <summary>
+	/// Calls a method in the solver script attached to |solver| to stop the current animated solution.
+	/// </summary>
 	public void Freeze () {
 		solver.GetComponent<Solver> ().StopSolve ();
 	}
