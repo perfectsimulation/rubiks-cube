4119d854b0bf47417bb8e8df15e69210535ad795
Sally Blair <sallyblair88@gmail.com>
2017-03-29 15:39:45 -0400

    Update Glide_Bcw.cs

--- Guide/Glide_Bcw.cs
+++ Guide/Glide_Bcw.cs
@@ -1,8 +1,15 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// This class animates its associated GameObject to show the user a hint to make a clockwise
+/// turn on the Yellow side.
+/// </summary>
 public class Glide_Bcw : MonoBehaviour {
 
+	/// <summary>
+	/// Update this instance by "animating" the GameObject this MonoBehaviour is attached to.
+	/// </summary>
 	void Update () {
 		Vector3 current = this.transform.position;
 		Vector3 target1 = new Vector3 (-1.5f, current.y, -1.5f);
